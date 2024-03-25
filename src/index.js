d827166192803b25b859033a6b40319952c9777e
Gabriella-01 <agboragabigabriella@gmail.com>
2024-03-25 11:58:34 +0100

    initialize node

---
+++ src/index.js
@@ -0,0 +1,6 @@
+var a; //Declare variable named a.
+a = 10; //Assign the integer value of 10 to variable a.
+b = 11; //Create variable c and assign a value to it. No previous declaration statement 
+var c = 12; //Declare variable c and assign it the value of 12 in one statement.
+
+console.log("a="+ a + ";b=" + b +";c="+ c)
